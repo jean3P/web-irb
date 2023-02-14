3f18c25fcf5d683e433d4d7900a1b22387a1502a
jprincipe6 <jeanpaul.principe@gmail.com>
2023-02-14 14:35:19 +0100

    feat: create button, navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,56 @@
+import React, {useState, useEffect} from "react";
+import {Link} from "react-router-dom";
+import './Navbar.css';
+import './Button'
+
+function Navbar(){
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+  const [button, setButton] = useState(true);
+  const showButton = () => {
+    if(window.innerWidth <= 960){
+      setButton(false)
+    }else{
+      setButton(true)
+    }
+  };
+
+  useEffect(() => {
+    showButton()
+  },[])
+  window.addEventListener('resize', showButton)
+
+  return (
+    <div>
+      <>
+        <nav className="navbar">
+          <div className="navbar-container">
+            <Link to={"/"} className={"navbar-logo"} onClick={closeMobileMenu}>
+              IRB <i className="fab fa-typo3"/>
+            </Link>
+            <div className={"menu-icon"} onClick={handleClick}>
+              <i className={click ? 'fas fa-times': 'fas fa-bars'} />
+            </div>
+            <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+              <li className={'nav-item'}>
+                <Link to={"/"} className={"nav-links"} onClick={closeMobileMenu}>Home</Link>
+              </li>
+              <li className={'nav-item'}>
+                <Link to={"/aboutUs"} className={"nav-links"} onClick={closeMobileMenu}>About us</Link>
+              </li>
+              <li className={'nav-item'}>
+                <Link to={"/schedule"} className={"nav-links"} onClick={closeMobileMenu}>News</Link>
+              </li>
+              <li className={'nav-item'}>
+                <Link to={"/contactUs"} className={"nav-links"} onClick={closeMobileMenu}>Contact us</Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </>
+    </div>
+  )
+}
+
+export default Navbar
